1f90bd08cadc7ac6e0d5f152c900474e6cf628bb
Valmond <valmondosmanaj@gmail.com>
2026-03-01 03:46:24 +0100

    Fixed an issue with CORS

--- backend/Program.cs
+++ backend/Program.cs
@@ -122,8 +122,7 @@ builder.Services.AddCors(options =>
     {
         policy.AllowAnyOrigin()
               .AllowAnyHeader()
-              .AllowAnyMethod()
-              .AllowCredentials();
+              .AllowAnyMethod();
 
     });
 });
